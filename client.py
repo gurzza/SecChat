72919be6592c41ef3c6d1f8e9c9950bbb40f4425
Gleb Zagovalski <gurzza@list.ru>
2023-10-23 20:02:07 +0300

    fix:multithreading add:started working with dgst

--- client.py
+++ client.py
@@ -1,22 +1,20 @@
+import hashlib
 import socket
 import socket
 import sys
+import ecdsa
 
 
-def create_my_name(sc):
+def create_my_name(sc, pk_sign):
     flagName = False
+    name = ''
 
+    sc.send(str(pk_sign.to_der()).encode())
     while not flagName:
         name = input(sc.recv(1024).decode())
         sc.send(name.encode())
         flagName = (sc.recv(1024).decode() == 'True')
-
-
-# def initiate_connection(client):
-#
-#     while
-#     recipient_name = input(client.recv(1024).decode())
-#     client.send(recipient_name.encode())
+    return name
 
 
 def print_operations():
@@ -27,40 +25,70 @@ def print_operations():
 
 
 def print_user_list(client):
-    pass
+    UserName = client.recv(2048).decode()
+    # print('Connected users:\n', ', '.join(UserName))
+    print('Connected users:\n', UserName)
+
+
+def initiate_connection(client):
+    flagRecipient = False
+    recipient_name = ''
+
+    while not flagRecipient:
+        recipient_name = input(client.recv(1024).decode())
+        client.send(recipient_name.encode())
+        flagRecipient = (client.recv(512) == 'True')
+
+    return recipient_name
+
+
+####################################
+def get_dgst_keys():
+    # sk
+    s = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256)
+    # pk
+    v = s.get_verifying_key()
+    return s, v
 
 
 if __name__ == '__main__':
     # create connection
     SERVER_NAME = 'DESKTOP-7FRD9J8'
     SERVER_IP = socket.gethostbyname(SERVER_NAME)
-    PORT = 8081
+    PORT = 8082
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #try:
+    # remove or keep try-except?
+    # try:
     client.connect((SERVER_IP, PORT))
-    #except:
+    # except:
     #   sys.exit()
     #########
 
     # log in
-    create_my_name(client)
+    sk_sign, pk_sign = get_dgst_keys()
+    client_name = create_my_name(client, pk_sign)
+    print('Welcome, ', client_name, '\n')
     #########
-
 
     operation = ''
     while 1:
         print_operations()
         operation = input()
         if operation.lower() == 'list' or operation.lower() == 'l':
-            client.send(operation.encode())
+            client.send(operation.lower()[0].encode())
+            # UserName = client.recv(2048).decode()
             print_user_list(client)
+        ###
         elif operation.lower() == 'connect' or operation.lower() == 'c':
-            client.send(operation.encode())
-            initiate_connection(client)
+            client.send(operation.lower()[0].encode())
+            rec_name = initiate_connection(client)
+            get_keys(client)
+        ###
         elif operation.lower() == 'quit' or operation.lower() == 'q':
-            client.send(operation.encode())
+            client.send(operation.lower()[0].encode())
             break
+        ###
         else:
             print('ERROR! NO SUCH OPERATION! TRY ONE MORE TIME...\n')
     client.close()
--- server.py
+++ server.py
@@ -3,71 +3,102 @@ import os
 import threading
 import hashlib
 from Crypto.Random import get_random_bytes
+import ecdsa
+
+# Create Socket (TCP) Connection
+ServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+HOSTNAME = socket.gethostname()
+HOST_IP = socket.gethostbyname(HOSTNAME)
+PORT = 8082
+ThreadCount = 0
+MAX_CONNECTIONS = 5
 
 UserName = []
+PK_DGST_KEYS = []
+SERVER_PK_DGST = ''
+SERVER_SK_DGST = ''
+
+try:
+    ServerSocket.bind((HOST_IP, PORT))
+except socket.error as e:
+    print(str(e))
+
+print('Waiting for a Connection..')
+ServerSocket.listen(MAX_CONNECTIONS)
 
 
-def create_user_name(connection):
+def create_user_profile(connection):
     flagName = False
+    name = ''
 
+    pk_key_client = connection.recv(1024).decode()
+    # print('pk_key_client:\n', pk_key_client)
     while not flagName:
         connection.send('(FROM SERVER) Enter your name/nickname: '.encode())
         name = connection.recv(1024).decode()
-        if not name in UserName:
+        if name not in UserName:
             flagName = True
             UserName.append(name)
+            PK_DGST_KEYS.append(pk_key_client)
+        connection.send(str(flagName).encode())
+
+    return name
 
 
 def choose_recipient(connection):
-    connection.send('(FROM SERVER) Enter the name you want to communicate with: ')
+    flagRecipient = False
+    recipient_name = ''
+
+    while not flagRecipient:
+        connection.send('(FROM SERVER) Enter the name you want to communicate with: ')
+        recipient_name = connection.recv(1024).decode()
+        flagRecipient = recipient_name in UserName
+        connection.send(str(flagRecipient).encode())
+
+    return recipient_name
+
+
+##########################################################
+def key_exchange(connection, first_client_name, second_client_name):
+    # sk
+    s = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256)
+    # pk
+    v = s.get_verifying_key()
+
+    # msg = '1234'.encode()
+    # dgst = s.sign(msg)
+    # try:
+    #     v.verify(dgst, '123'.encode())
+    # except:
+    #     print('ERROR!')
 
 
 def threaded_client(connection):
-    create_user_name(connection)
+    first_client_name = create_user_profile(connection)
     ######
-    operation = connection.recv(1024).decode()
+    operation = connection.recv(512).decode()
+    while operation != 'q':
+        if operation == 'l':
+            connection.send(str(UserName).encode())
+        else:  # == 'c'
+            second_client_name = choose_recipient(connection)
+            key_exchange(connection, first_client_name, second_client_name)
 
 
+###########################################################
 
-    #choose_recipient(connection)
+SERVER_SK_DGST = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256)
+SERVER_PK_DGST = SERVER_SK_DGST.get_verifying_key()
 
-
-    client_data = connection.recv(1024).decode()
-    while client_data.upper() != 'QUIT' and client_data.upper() != 'Q':
-        client_data = connection.recv(2048).decode()
-
-
-#
-#
-#
-
-
-if __name__ == "__main__":
-    # Create Socket (TCP) Connection
-    ServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    HOSTNAME = socket.gethostname()
-    HOST_IP = socket.gethostbyname(HOSTNAME)
-    PORT = 8081
-    ThreadCount = 0
-    MAX_CONNECTIONS = 5
-
-    try:
-        ServerSocket.bind((HOST_IP, PORT))
-    except socket.error as e:
-        print(str(e))
-
-    print('Waiting for a Connection..')
-    ServerSocket.listen(5)
-
-    while True:
-        Client, address = ServerSocket.accept()
-
-        client_handler = threading.Thread(
-            target=threaded_client,
-            args=Client
-        )
-        client_handler.start()
-        print('Connection Request: ' + str(ThreadCount))
-        if ThreadCount == 0:
-            break
-    ServerSocket.close()
+while True:
+    Client, address = ServerSocket.accept()
+    client_handler = threading.Thread(
+        target=threaded_client,
+        args=(Client,)
+    )
+    client_handler.start()
+    ThreadCount += 1
+    print('Connection Request: ' + str(ThreadCount))
+    if ThreadCount == 0:
+        break
+ServerSocket.close()
